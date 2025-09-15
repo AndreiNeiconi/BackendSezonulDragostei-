0ba56504f62eedeaf2e31b4994e0381c0d39cfdb
Neiconi Andrei <a.neiconi19@gmail.com>
2025-09-15 18:29:36 +0300

    test

--- subscribe-api.js
+++ subscribe-api.js
@@ -183,6 +183,12 @@ Details: ${details}
   }
 });
 
+app.post("/api/test", async (req, res) => {
+  message: {
+    ("works");
+  }
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
